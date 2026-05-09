8a3a25759b0ea6376550ec8ddca3ada295fa25f2
chaitanya-encegen <chaitanya.encegen@gmail.com>
2026-05-10 00:05:02 +0530

    Updated production API URL

--- cc96-service-platform/client/src/services/api.js
+++ cc96-service-platform/client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://cc96-service-platform.onrender.com/api",
 });
 
 export default API;
