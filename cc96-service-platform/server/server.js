f05b2de8fefae7126bf420026ab7b2ba7d3541da
chaitanya-encegen <chaitanya.encegen@gmail.com>
2026-05-10 14:56:57 +0530

    fixed vendor

--- cc96-service-platform/server/server.js
+++ cc96-service-platform/server/server.js
@@ -1,54 +1,40 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-
-const sequelize = require("./config/db");
-
-require("./models");
-
-const authRoutes = require("./routes/authRoutes");
-const serviceRoutes = require("./routes/serviceRoutes");
-const seedServices = require("./utils/seedServices");
-const bookingRoutes = require("./routes/bookingRoutes");
-
-const app = express();
-
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://cc96-service-platform.vercel.app",
-    ],
-    credentials: true,
-  })
-);
-app.use(express.json());
-
-app.use("/api/auth", authRoutes);
-app.use("/api/services", serviceRoutes);
-app.use("/api/bookings", bookingRoutes);
-
-
-app.get("/", (req, res) => {
-  res.send("API Running");
-});
-
 sequelize.authenticate()
-.then(() => {
+.then(async () => {
 
-  console.log("MySQL Connected");
+  console.log(
+    "PostgreSQL Connected"
+  );
 
-  sequelize.sync({ force: true })
-  .then(() => {
+  try {
 
-    console.log("Database Synced");
+    await sequelize.sync({
+      force: true,
+    });
 
-    seedServices();
-  });
-});
+    console.log(
+      "Database Synced"
+    );
 
-const PORT = process.env.PORT || 5000;
+    await seedServices();
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+    console.log(
+      "Services Seeded"
+    );
+
+  } catch (error) {
+
+    console.log(
+      "SYNC ERROR:",
+      error
+    );
+  }
+
+})
+.catch((error) => {
+
+  console.log(
+    "DB CONNECTION ERROR:",
+    error
+  );
+
 });
